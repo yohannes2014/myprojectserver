63f8e59b9b4b83aa844f8cfc5b5ec7494f8900ae
jossysite <106884670+yohannes2014@users.noreply.github.com>
2024-11-30 11:23:31 +0300

    env document add

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const musicRouter = require('./src/routes/musicRoutes.js');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 
 
 const app = express();
 app.use(cors());
+dotenv.config(); 
  
 app.use(express.static('uploads'));   
 app.use(express.json()); 
@@ -15,7 +17,7 @@ app.use('/api',musicRouter);
 const port = process.env.PORT || 7000;
 
  
-mongoose.connect("mongodb+srv://myohannes2010:qdlffXL21aL4r9t5@yohannesmanaye.ionwx.mongodb.net/myproject").then
+mongoose.connect(process.env.MONGO_URL).then
 (()=>console.log(`connected to database`)).catch
 ((err)=>{console.log(`err:${err}`)}); 
 
